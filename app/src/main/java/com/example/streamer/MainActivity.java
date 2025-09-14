5c0f2e00a6914e719c5137eedc95d1fde260b324
feminkk@gmail.com <feminkk@gmail.com>
2025-09-14 14:08:37 +0530

    autoplay

--- app/src/main/java/com/example/streamer/MainActivity.java
+++ app/src/main/java/com/example/streamer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.streamer;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
@@ -16,37 +17,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Fullscreen
+        // Keep fullscreen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        // Keep screen on while in foreground
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         webView = new WebView(this);
+        webView.setBackgroundColor(Color.BLACK);
         setContentView(webView);
 
         webView.setWebViewClient(new WebViewClient());
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false); // allow autoplay
 
-        // Load your URL
         webView.loadUrl("http://192.168.0.103:5945");
-    }
-
-    @Override
-    public void onBackPressed() {
-        if(webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        // Exit completely when app is minimized
-        finishAffinity(); // close all activities
-        System.exit(0);   // kill process
+        // Destroy WebView when app goes to background
+        if (webView != null) {
+            webView.loadUrl("about:blank");
+            webView.stopLoading();
+            webView.onPause();
+            webView.removeAllViews();
+            webView.destroy();
+            webView = null;
+        }
+        finish(); // close the activity
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Back button exits immediately
+        finish();
     }
 }
 
